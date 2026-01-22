f94b7c28b55ba2f7663e2284d0f75c061bf94b66
Leo Hepting <leohepting@web.de>
2026-01-22 14:37:32 +0100

    feat(todo-list-frontend): services

---
+++ frontend/todo-app/src/services/TodoListServices.ts
@@ -0,0 +1,15 @@
+import type { TodoList } from "@/types/TodoList";
+import type { createApiService } from "./apiService";
+import {Routes, ServerAddress} from "../config/serverConfig"
+
+export function createTodoListService(api: ReturnType<typeof createApiService>) {
+  return {
+    getTodoLists: () => api.get<TodoList[]>(`${ServerAddress}${Routes.GET_LISTS}`),
+
+    /* getUserById: (id: number) => api.get<User>(`/users/${id}`),
+
+    createUser: (dto: CreateUserDto) => api.post<User>("/users", dto),
+
+    deleteUser: (id: number) => api.delete<void>(`/users/${id}`), */
+  };
+}
